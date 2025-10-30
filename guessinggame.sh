9d1e1d872604679de483e206d77c8e66ff8469a3
sou2386 <sassisousou63@gmail.com>
2025-10-30 12:35:59 +0100

    guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+function get_file_count{
+    echo $(ls -1 | wc -l)
+}
+file_count = $(get_file_count)
+guess = -1
+
+echo "Welcome to the Guessing Game!"
+echo "Can you guess how many files are in the current directory?"
+
+while [[ $guess -ne $file_count ]]
+do
+    read -p "Enter your guess: " guess 
+    if [[ $guess -lt $file_count ]]
+    then
+        echo "Too low! Try again."
+    elif [[ $guess -gt $file_count ]]
+    then 
+        echo "Too high! Try again."
+    else
+        echo "Congratulations! You guessed it right."
+    fi
+done
